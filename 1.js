f2a059fdef4415dd918d17a06bff8a0eb366007c
Jun Li <lijunray@me.com>
2017-08-31 10:26:29 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -4,3 +4,7 @@ const list = [1, 2, 3, 4, 5];
 const mapExample = list.map(element => element * 2); // [2, 4, 6, 8, 10]
 const reduceExample = list.reduce((newList, element) => { newList.push(element * 2); }, []); // [2, 4, 6, 8, 10]
 
+// More general example:
+list.map((element, i) => f(element, i));
+// Same as
+list.reduce((newList, element, i) => { newList.push(f(element, i)); }, []);
